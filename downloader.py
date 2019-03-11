49c641ca7dadf359f514b32b65b2795eddfc3111
Mohammad Mohsen-Pour <mohammad.mohsenpour1@gmail.com>
2019-03-11 16:00:13 -0400

    changed the format from csv to tsv

--- downloader.py
+++ downloader.py
@@ -1,7 +1,8 @@
 import tweepy
-import creds
+import creds2
 import emoji
 import interesting_labels
+import time
 
 def remove_word_starting_with(character,text):
     '''
@@ -29,7 +30,7 @@ def is_useful(tweet_text):
 def cleanup(tweet_text):
     '''cleans the tweet text by removing new lines
      emojis and mentions(any word starting with @) '''
-    tweet_text = tweet_text.replace('\n', ' ').replace(',',' ')
+    tweet_text = tweet_text.replace('\n', ' ')
     tweet_text = remove_word_starting_with('@',tweet_text)
     tweet_text = remove_emoji(tweet_text)
     return tweet_text
@@ -42,23 +43,23 @@ def tweeter_api(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET):
     return api
 
 if __name__ == '__main__':
-    api = tweeter_api(creds.CONSUMER_KEY,
-                      creds.CONSUMER_SECRET,
-                      creds.ACCESS_TOKEN,
-                      creds.ACCESS_TOKEN_SECRET)
+    api = tweeter_api(creds2.CONSUMER_KEY,
+                      creds2.CONSUMER_SECRET,
+                      creds2.ACCESS_TOKEN,
+                      creds2.ACCESS_TOKEN_SECRET)
     # open the training data file
     with open('full_train_plaintext.txt','r') as training_file:
         with open('processed.txt', 'a') as tweet_text_file:
             i=0
             for line in training_file:
-                if i>1000:
+                if i>500:
                     exit(0)
                 try:
+                    time.sleep(1)
                     # get the status id from the file
                     status_id = line.split()[0]
                     # get the emoji labels from the file
                     labels = (line.split()[1]).split(',')
-
                     # get the tweet using twitte api
                     tweet = api.get_status(status_id)
 
@@ -66,8 +67,15 @@ if __name__ == '__main__':
                     tweet_text = cleanup(tweet.text)
                     if is_useful(tweet_text):
                         for label in labels:
-                            print('id: ', status_id, ' text: ', tweet_text, ' labels: ', labels)
-                            tweet_text_file.write(status_id +','+ tweet_text +','+ label + "\n")
-                            i+=1
+                            if(int(label) in interesting_labels.wanted_list):
+                                print('id: ', status_id, ' text: ', tweet_text, ' labels: ', labels)
+                                tweet_text_file.write(status_id +'\t'+ tweet_text +'\t'+ label + "\n")
+                                print('i: ', i)
+                                i+=1
                 except BaseException as e:
+                    if i>500:
+                        exit(0)
                     print(e)
+                    print('i: ', i)
+                    i+=1
+
