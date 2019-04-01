bb3596afd0bc6abc6766d1df3a3767d172cb83c5
Asad <asadjamil1998@gmail.com>
2019-04-01 16:15:30 -0400

    Added forms.py that handles the input entered by the user.

---
+++ Interface/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+# for debugging
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class InputForm(FlaskForm):
+    tweet = StringField('Enter a sentence:',
+                        validators=[DataRequired()])
+    submit = SubmitField('Enter')
